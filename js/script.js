76489f0cd41b49e124b1185bea48a738878a25d0
Andy Humes <andrew.humes@du.edu>
2021-11-13 20:49:25 -0700

    submit

--- js/script.js
+++ js/script.js
@@ -1,8 +1,38 @@
 const blue = $('#blue');
+let questionTwo = $('#questionTwo');
+questionTwo.css('display', 'none');
+
+let questionThree = $('#questionThree');
+questionThree.css('display', 'none');
 
 
 blue.on('click', function()
-{alert('You clicked')});
+{alert('Correct');
+blue.css('background-color', 'green');
+questionTwo.css('display', 'block')
+localStorage.setItem('highScore', JSON.stringify('correct'));
+
+});
+
+let fortyTwo = $('#fortyTwo');
+fortyTwo.on('click', function ()
+{alert('Correct');
+fortyTwo.css('background-color', 'green');
+questionThree.css('display', 'block');
+localStorage.setItem('highScore', JSON.stringify('sgaddgs'));
+});
+
+
+let andy = $('#andy');
+andy.on('click', function ()
+{alert('Correct');
+andy.css('background-color', 'green');
+questionThree.css('display', 'block')});
+
+
+let highScore = $('#highScore');
+
+
 
 let navBarText = $('.navbar-text');
 
